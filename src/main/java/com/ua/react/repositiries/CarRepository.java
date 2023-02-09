b0d60e993cddcb733836e4d94b74d655d33d0e47
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2023-02-09 13:43:44 +0200

    repo with query

--- src/main/java/com/ua/react/repositiries/CarRepository.java
+++ src/main/java/com/ua/react/repositiries/CarRepository.java
@@ -1,7 +1,10 @@
 package com.ua.react.repositiries;
 
 import com.ua.react.model.Car;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.*;
 
 /**
  * CarRepository.
@@ -10,5 +13,19 @@ import org.springframework.data.repository.CrudRepository;
  * @version 5.0
  * @since 09/02/2023
  */
+@Repository
 public interface CarRepository extends CrudRepository<Car, Long> {
+    //  @Query("select c from Car c where c.brand like %?1")
+    @Query("select c from Car c where c.brand = ?1")
+    List<Car> findByBrand(String brand);
+
+    List<Car> findByBrandAndModel(String brand, String model);
+
+    List<Car> findByColor(String color);
+
+    List<Car> findByBrandOrColor(String brand, String color);
+
+    List<Car> findByYear(int year);
+
+    List<Car> findByBrandOrderByYearAsc(String brand);
 }
--- src/main/java/com/ua/react/repositiries/OwnerRepository.java
+++ src/main/java/com/ua/react/repositiries/OwnerRepository.java
@@ -2,6 +2,7 @@ package com.ua.react.repositiries;
 
 import com.ua.react.model.Owner;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * OwnerRepository.
@@ -10,5 +11,6 @@ import org.springframework.data.repository.CrudRepository;
  * @version 5.0
  * @since 09/02/2023
  */
+@Repository
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 }
