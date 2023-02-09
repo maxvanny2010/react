b30ed713eeb21a887f101217da3637d24c7e4208
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2023-02-09 21:58:52 +0200

    react counters States batch

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,15 +1,18 @@
 import React, {useState} from 'react';
 
-function Counter() {
+function App() {
     const [count, setCount] = useState(0);
+    const [count2, setCount2] = useState(0);
+    const increment = () => {
+        setCount(count + 1); // No re-rendering yet
+        setCount2(count2 + 1);// Component re-renders after all state updates
+    }
     return (
         <div>
-            <p>Counter = {count}</p>
-            <button onClick={() => setCount(count + 1)}>
-                Increment
-            </button>
+            <p>Counters: {count} {count2}</p>
+            <button onClick={increment}>Increment</button>
         </div>
     );
 }
 
-export default Counter;
+export default App;
