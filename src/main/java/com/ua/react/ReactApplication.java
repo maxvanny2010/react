82243c444c2bcbe444d9ed6a3420dd2634ba389f
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2023-02-09 10:54:07 +0200

    logger

--- src/main/java/com/ua/react/ReactApplication.java
+++ src/main/java/com/ua/react/ReactApplication.java
@@ -1,13 +1,17 @@
 package com.ua.react;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReactApplication {
+    private static final Logger logger = LoggerFactory.getLogger(ReactApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(ReactApplication.class, args);
+        logger.info("Application started");
     }
 
 }
