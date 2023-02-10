c06acbf761a2e01abda664143a6bbfed7912ed07
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2023-02-10 22:04:21 +0200

    carfront init

---
+++ carfront/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+function App() {
+    return (
+        <div className="App">
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6"> CarShop </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
+export default App;
