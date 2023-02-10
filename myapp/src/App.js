f2035c0a6de6dfa77ed4fab9bed31837098bf86d
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2023-02-10 10:56:41 +0200

    react counters useEffect print each render side effect by line

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,10 +2,10 @@ import React, {useEffect, useState} from 'react';
 
 function Counter() {
     const [count, setCount] = useState(0);
-// Called after every render
+// Runs when count value is changed and component is  re-rendered
     useEffect(() => {
-        console.log('Hello from useEffect')
-    });
+        console.log('Counter value is now ' + count);
+    }, [count])
     return (
         <div>
             <p>{count}</p>
