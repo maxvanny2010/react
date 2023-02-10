6f8c3b5374a3f7cf64a5d92ab1d0b6b1e4846077
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2023-02-10 16:30:04 +0200

    react GithubFetch fetch to table

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Weather from "./Weather";
+import GithubFetch from "./GithubFetch";
 
 function App() {
-    return <Weather city="Zurich"/>;
+    return <GithubFetch/>;
 }
 
 export default App;
---
+++ myapp/src/GithubFetch.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function GithubFetch() {
+    const [keyword, setKeyword] = useState('');
+    const [data, setData] = useState([]);
+    const fetchData = () => {
+        fetch(`https://api.github.com/search/repositories?q=${keyword}`)
+            .then(response => response.json())
+            .then(data => setData(data.items))
+            .catch(err => console.error(err))
+    }
+    return (
+        <div className="App">
+            <input value={keyword}
+                   onChange={e => setKeyword(e.target.value)}/>
+            <button onClick={fetchData}>Fetch</button>
+            <table>
+                <tbody>
+                {
+                    data.map(repo =>
+                        <tr key={repo.id}>
+                            <td>{repo.full_name}</td>
+                            <td>
+                                <a href={repo.html_url}>{repo.html_url}</a>
+                            </td>
+                        </tr>
+                    )
+                }
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default GithubFetch;
