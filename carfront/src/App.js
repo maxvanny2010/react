be7a13f5e719cecbae7be32ee0370379520f059e
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2023-02-10 22:05:58 +0200

    carfront container

--- carfront/src/App.js
+++ carfront/src/App.js
@@ -2,16 +2,20 @@ import './App.css';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import {Container} from "@mui/material";
 
 function App() {
     return (
-        <div className="App">
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6"> CarShop </Typography>
-                </Toolbar>
-            </AppBar>
-        </div>
+        <Container>
+            <div className="App">
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h6"> CarShop </Typography>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        </Container>
     );
 }
+
 export default App;
