866e765529b403f3eae7417f4d4066ca0178847c
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2023-02-11 12:27:39 +0200

    carfront  AddCar

--- carfront/src/component/Carlist.js
+++ carfront/src/component/Carlist.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {SERVER_URL} from "../constants";
 import {DataGrid} from '@mui/x-data-grid';
 import Snackbar from '@mui/material/Snackbar';
+import AddCar from "./AddCar";
 
 function CarList() {
     const [open, setOpen] = useState(false);
@@ -41,24 +42,39 @@ function CarList() {
                 .catch(err => console.error(err))
         }
     }
-
+    const addCar = (car) => {
+        console.log(car);
+        fetch(SERVER_URL + '/api/vehicles',
+            {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(car)
+            })
+            .then(response => {
+                if (response.ok) fetchCars();
+                else alert('Something went wrong!');
+            })
+            .catch(err => console.error(err))
+    }
     useEffect(() => {
         fetchCars();
     }, []);
     return (
-        <div style={{height: 500, width: '100%'}}>
-            <DataGrid
-                rows={cars}
-                columns={columns}
-                disableSelectionOnClick={true}
-                getRowId={row => row._links.self.href}/>
-            <Snackbar
-                open={open}
-                autoHideDuration={2000}
-                onClose={() => setOpen(false)}
-                message="Car deleted"/>
-        </div>
-
+        <React.Fragment>
+            <div style={{height: 500, width: '100%'}}>
+                <DataGrid
+                    rows={cars}
+                    columns={columns}
+                    disableSelectionOnClick={true}
+                    getRowId={row => row._links.self.href}/>
+                <Snackbar
+                    open={open}
+                    autoHideDuration={2000}
+                    onClose={() => setOpen(false)}
+                    message="Car deleted"/>
+            </div>
+            <AddCar addCar={addCar}/>
+        </React.Fragment>
     );
 }
 
