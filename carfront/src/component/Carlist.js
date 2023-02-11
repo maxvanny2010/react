ec38893c4cc32ba33e0193f2b662a4a9b53bbde1
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2023-02-11 11:25:24 +0200

    carfront  DataGrid a button delete by row.id

--- carfront/src/component/Carlist.js
+++ carfront/src/component/Carlist.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {SERVER_URL} from "../constants";
-import { DataGrid } from '@mui/x-data-grid';
+import {DataGrid} from '@mui/x-data-grid';
 
 function CarList() {
     const [cars, setCars] = useState([]);
@@ -10,15 +10,32 @@ function CarList() {
         {field: 'color', headerName: 'Color', width: 200},
         {field: 'year', headerName: 'Year', width: 150},
         {field: 'price', headerName: 'Price', width: 150},
+        {
+            field: '_links.self.href',
+            headerName: '',
+            sortable: false,
+            filterable: false,
+            renderCell: row =>
+                <button onClick={() => onDelClick(row.id)}>Delete</button>
+        }
     ];
-    useEffect(() => {
+    const fetchCars = () =>
         fetch(SERVER_URL + '/api/vehicles')
             .then(response => response.json())
             .then(data => setCars(data._embedded.cars))
             .catch(err => console.error(err));
+
+    const onDelClick = (url) => {
+        fetch(url, {method: 'DELETE'})
+            .then(response => fetchCars())
+            .catch(err => console.error(err))
+    }
+
+    useEffect(() => {
+        fetchCars();
     }, []);
-    return(
-        <div style={{ height: 500, width: '100%' }}>
+    return (
+        <div style={{height: 500, width: '100%'}}>
             <DataGrid
                 rows={cars}
                 columns={columns}
