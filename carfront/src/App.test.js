8d9744ea7e94d809cd082af862ff41d8e9dc2731
maxvanny2010 <39569512+maxvanny2010@users.noreply.github.com>
2023-02-11 15:03:13 +0200

    carfront  Test

--- carfront/src/App.test.js
+++ carfront/src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('open add car modal form', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  fireEvent.click(screen.getByText('New Car'));
+  expect(screen.getByRole('dialog')).toHaveTextContent('New car');
 });
